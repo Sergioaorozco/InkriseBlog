4011a241391ebc2a46e85f6145bf09089812d597
Sergio <sergioa.orozcoo@gmail.com>
2022-10-20 18:03:54 -0500

    Change images routes

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -40,7 +40,7 @@
           <article class="lg:flex-row gap-5 flex flex-col">
             <figure class="rounded-xl overflow-hidden">
               <img
-                :src="post.image"
+                :src="require(post.image)"
                 class="
                   w-full
                   h-full
