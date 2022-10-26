e817828301470e1190c077185540a0f04e90ec53
Sergio <sergioa.orozcoo@gmail.com>
2022-10-26 15:53:18 -0500

    Fix breadcrumb icons margins

--- src/views/BlogContent.vue
+++ src/views/BlogContent.vue
@@ -7,8 +7,8 @@
           <ol class="inline-flex items-center space-x-1 md:space-x-3">
             <li>
               <div class="flex items-center">
-                <svg class="w-4 h-4 mr-2" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                <router-link to="/blog" class="ml-1 text-sm font-medium text-purple-500 hover:text-purple-400 md:ml-2">
+                <svg class="w-4 h-4 mr-2" fill="#A855F7" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                <router-link to="/blog" class="text-sm font-medium text-purple-500 hover:text-purple-400 md:ml-2">
                 Blog
                 </router-link>
               </div>
