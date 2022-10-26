ad9a88bee1e1d20559a288b5567240eb79e95aa3
Sergio <sergioa.orozcoo@gmail.com>
2022-10-26 17:56:04 -0500

    fix images styles from butterCMS

--- src/views/BlogContent.vue
+++ src/views/BlogContent.vue
@@ -16,7 +16,7 @@
             <li aria-current="page">
               <div class="flex items-center">
                 <svg class="w-6 h-6 text-white" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                <span class="ml-1 text-sm font-medium text-white md:ml-2">{{postContent.data.title}}</span>
+                <span  class="ml-1 text-sm font-medium text-white md:ml-2">{{postContent.data.title}}</span>
               </div>
             </li>
           </ol>
@@ -27,7 +27,7 @@
       </div>
     </section>
     <figure class="wrapper px-5">
-      <img :src="postContent.data.featured_image"  class="rounded-3xl h-30 md:h-[400px] w-full object-cover mb-4 -mt-28 sm:-mt-40" alt="">
+      <img id="featureImage" :src="postContent.data.featured_image"  class="rounded-2xl h-30 md:h-[400px] w-full object-cover mb-4 -mt-28 sm:-mt-40" alt="">
     </figure>
       <div class="single-post wrapper px-5" v-html="postContent.data.body"></div>
   </div>
@@ -70,4 +70,8 @@ created() {
   margin-block: 1em;
 }
 
+:deep() img:not(#featureImage) {
+margin-block: 1em;
+border-radius: 0.5em;
+}
 </style>
