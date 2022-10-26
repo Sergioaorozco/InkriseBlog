b7f593d0681cc796a74ae0697cd13d6e170a9695
Sergio <sergioa.orozcoo@gmail.com>
2022-10-25 21:22:46 -0500

    Fix route and name

--- src/views/BlogContent.vue
+++ src/views/BlogContent.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <section v-for="post in PostContent" :key="post.title">
-      <h1>{{post.data.title}}</h1>
-      <div v-html="post.data.body"></div>
+      <h1>{{post.data.data.title}}</h1>
+      <div v-html="post.data.data.body"></div>
     </section>
   </div>
 </template>
@@ -11,7 +11,6 @@ import Butter from 'buttercms';
 const butter = Butter('c4132363d0ae8e6bdd024c99dc21de914f4b8cbd');
 
 export default {
-name:'blog-content',
 data()   {
   return {
     postContent: null
@@ -21,7 +20,7 @@ methods: {
   getPost() {
     butter.post.retrieve(this.$route.params.slug)
     .then(res => {
-      this.postContent = res.data
+      this.postContent = res.data.data
       console.log(res)
     }). catch( res =>{
       console.log(res)
