87d4d30b8cb9f0c8d194f6c9c9a7da109d71a626
Sergio <sergioa.orozcoo@gmail.com>
2022-10-26 09:49:50 -0500

    Show single post content

--- src/views/BlogContent.vue
+++ src/views/BlogContent.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <section v-for="post in PostContent" :key="post.title">
-      <h1>{{post.data.data.title}}</h1>
-      <div v-html="post.data.data.body"></div>
+  <div id="blog-content">
+    <h1>hghghg</h1>
+    <section class="wrapper">
+      <h1>{{postContent.data.title}}</h1>
+      <div v-html="postContent.data.body"></div>
     </section>
   </div>
 </template>
@@ -11,16 +12,17 @@ import Butter from 'buttercms';
 const butter = Butter('c4132363d0ae8e6bdd024c99dc21de914f4b8cbd');
 
 export default {
+name: 'blog-content',
 data()   {
   return {
-    postContent: null
+    postContent: {}
   }
 },
 methods: {
   getPost() {
     butter.post.retrieve(this.$route.params.slug)
     .then(res => {
-      this.postContent = res.data.data
+      this.postContent = res.data
       console.log(res)
     }). catch( res =>{
       console.log(res)
